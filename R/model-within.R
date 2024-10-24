0b3ace36ab87539fa202e5165ef624a9cef05e1a
Erick <eral.th07@gmail.com>
2024-10-24 11:18:41 +0300

    update data processing function

--- R/model-within.R
+++ R/model-within.R
@@ -101,48 +101,39 @@ log_mlik_all <- function(stars_obj, membership, formula, family = "normal", corr
 #' This function preprocesses the data from a stars object by subsetting based on a cluster
 #' and converting the data to a long format with proper indexing for time and spatial location.
 #'
-#' @param stars_obj A stars object containing spatial data.
-#' @param k The cluster number to subset the data for.
-#' @param membership A vector or matrix that defines cluster membership for each region.
-#' @param time_var The name of the time variable (default is 'time').
+#' @param x A stars object containing spatial-temporal dimensions defined in `stnames`.
+#' @param k The cluster number to subset.
+#' @param membership A vector defining the cluster membership for each region.
+#' @param stnames The names of the `spatial` and `temporal` dimensions.
 #'
-#' @return A long-format data frame with ids for spatial and time indexing, suitable for INLA.
+#' @return A long-format data frame with ids for spatial and time indexing.
 #' @examples
-#' preprocess_data_each(stars_obj, k = 1, membership, time_var = "time")
+#' preprocess_data_each(x, k = 1, membership, time_var = "time")
 #'
 #' @export
-preprocess_data_each <- function(stars_obj, k, membership, time_var = "time") {
-  # Check if the input is a stars object
-  if (!inherits(stars_obj, "stars")) {
+preprocess_data_each <- function(x, k, membership, stnames = c("geometry", "time")) {
+  # Check if the input is a stars object and dimension names
+  if (!inherits(x, "stars")) {
     stop("Input must be a 'stars' object.")
   }
-
-  # Get the indices of the regions that belong to the kth cluster
-  cluster_regions <- which(membership == k)
-  dims <- st_dimensions(stars_obj)
-  space_dim_pos <- which(names(dims) == time_var)
-  if (is.vector(membership)) {
-
-    # Subset the stars object to only include the regions in the kth cluster
-    stars_obj_subset <- stars_obj[, ,cluster_regions, drop = FALSE]
+  if (any(!(stnames %in% dimnames(x)))) {
+    stop("Provided dimension names not found in starts object `x`.")
   }
 
-  # Convert the subsetted stars object to a long data frame
-  data_long <- as.data.frame(stars_obj_subset, long = TRUE)
+  # Subset cluster k
+  cluster <- which(membership == k)
+  x <- dplyr::filter(x, !!as.name(stnames[1]) %in% cluster)
 
-  # Calculate the number of time points (nt) and spatial locations (nk) in the cluster
-  nt <- length(unique(data_long[[time_var]]))  # number of time points
-  nk <- length(cluster_regions)  # number of regions in the kth cluster
+  # Spatio-temporal dimenstion in long format
+  dims <- expand_dimensions(x)
+  dims[[stnames[1]]] <- seq_along(dims[[stnames[1]]])
+  dims[[stnames[2]]] <- order(dims[[stnames[2]]])
+  dims <- setNames(do.call(expand.grid, dims)[stnames], c("ids", "idt"))
 
-  # Create id, idt, and ids ensuring correct indexing based on spatial locations and time
-  data_long$id <- seq_len(nt * nk)  # Create unique IDs for each observation
-  data_long$idt <- rep(seq_len(nt), time = nk)  # Create time index (idt) for each region
-  data_long$ids <- rep(seq_len(nk), each = nt)  # Create spatial index (ids) for each time point
-
-  # Remove the first column (if it contains geometry or spatial information)
-  data_inla <- data_long[, -1]
-
-  return(data_inla)  # Return the processed data for the kth cluster
+  # Merge dimensions and dataframe
+  x <- as.data.frame(x)
+  x[[stnames[1]]] <- NULL
+  cbind(list(id = 1:nrow(dims)), dims, x)
 }
 
 ##############################################################
