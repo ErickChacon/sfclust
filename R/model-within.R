157b73c3ac61995fb4fef88c6e487a0b06d4cb25
Erick <eral.th07@gmail.com>
2024-10-29 10:19:34 +0300

    update data_each, documentation, imports

--- R/model-within.R
+++ R/model-within.R
@@ -13,7 +13,7 @@
 #' "binomial", and "nbinomial" (negative binomial variant 1); defaults to "normal".
 #' @param correction Logical, indicating whether a correction should be applied to the computed log marginal
 #' likelihood; defaults to FALSE.
-#' @param time_var The name of the time variable in the stars object.
+#' @param stnames The name of the time variable in the stars object.
 #' @param N_var The name of the variable used as exposure or trials (for Poisson, binomial, etc.).
 #' @param detailed Logical, specifying whether to return the full model object or just the log marginal
 #' likelihood; defaults to FALSE.
@@ -31,8 +31,8 @@
 #' result <- log_mlik_each(1, stars_obj, membership, formula = formula, family = "poisson", correction = TRUE)
 #' }
 #' @export
-log_mlik_each <- function(k, stars_obj, membership, formula, family = "normal", correction = FALSE, time_var, N_var, detailed = FALSE, ...) {
-  inla_data <- data_each(stars_obj, k, membership, time_var = time_var)
+log_mlik_each <- function(k, stars_obj, membership, formula, family = "normal", correction = FALSE, stnames, N_var, detailed = FALSE, ...) {
+  inla_data <- data_each(stars_obj, k, membership, stnames = stnames)
   if(!is.null(N_var)){N <- get(N_var, envir = as.environment(stars_obj))}
   if (family == "poisson") {
     model <- INLA::inla(formula, family,
@@ -81,7 +81,7 @@ log_mlik_each <- function(k, stars_obj, membership, formula, family = "normal", 
 #' @param formula An object of class \code{\link[stats]{formula}} specifying the model to be used in INLA.
 #' @param family Character string specifying the family of distributions to use for the model. Defaults to "normal".
 #' @param correction Logical indicating whether a correction for dispersion or other factors should be applied. Defaults to FALSE.
-#' @param time_var The name of the time variable in the stars object.
+#' @param stnames The name of the time variable in the stars object.
 #' @param N_var The name of the variable used as exposure or trials (for Poisson, binomial, etc.).
 #' @param ... Additional arguments passed to the underlying `log_mlik_each` function.
 #'
@@ -89,17 +89,20 @@ log_mlik_each <- function(k, stars_obj, membership, formula, family = "normal", 
 #'
 #' @examples
 #' \dontrun{
-#' log_mlikelihoods <- log_mlik_all(stars_obj, membership, family = "poisson", formula = Y ~ temperature + precipitation, time_var = "time", N_var = "expected_cases")
+#' log_mlikelihoods <- log_mlik_all(stars_obj, membership, family = "poisson",
+#'   formula = Y ~ temperature + precipitation, stnames = "time", N_var = "expected_cases")
 #' }
 #' @export
-log_mlik_all <- function(stars_obj, membership, formula, family = "normal", correction = FALSE, time_var, N_var, ...) {
+log_mlik_all <- function(stars_obj, membership, formula, family = "normal", correction = FALSE, stnames, N_var, ...) {
   k <- max(membership)
-  sapply(1:k, log_mlik_each, stars_obj, membership, formula, family, correction, time_var, N_var, ...)
+  sapply(1:k, log_mlik_each, stars_obj, membership, formula, family, correction, stnames, N_var, ...)
 }
 
 ########################################################################################################################
-#' This function preprocesses the data from a stars object by subsetting based on a cluster
-#' and converting the data to a long format with proper indexing for time and spatial location.
+#' Prepare data for a cluster
+#'
+#' Subset a spatio-temporal dataset for a cluster and convert it to a long format with
+#' indices for time and spatial location.
 #'
 #' @param x A stars object containing spatial-temporal dimensions defined in `stnames`.
 #' @param k The cluster number to subset.
@@ -109,9 +112,17 @@ log_mlik_all <- function(stars_obj, membership, formula, family = "normal", corr
 #' @return A long-format data frame with ids for spatial and time indexing.
 #'
 #' @examples
-#' \dontrun{
-#' data_each(x, k = 1, membership = c(1, 1, 1, 2, 2))
-#' }
+#'
+#' dims <- st_dimensions(
+#'   geometry = st_sfc(lapply(1:5, function(i) st_point(c(i, i)))),
+#'   time = seq(as.Date("2024-01-01"), by = "1 day", length.out = 3)
+#' )
+#' x <- st_as_stars(cases = array(1:15, dim = c(5, 3)), dimensions = dims)
+#'
+#' data_each(x, k = 2, membership = c(1, 1, 1, 2, 2))
+#'
+#' @import cubelyr stars
+#' @importFrom dplyr filter
 #'
 #' @export
 data_each <- function(x, k, membership, stnames = c("geometry", "time")) {
@@ -125,7 +136,7 @@ data_each <- function(x, k, membership, stnames = c("geometry", "time")) {
 
   # Subset cluster k
   cluster <- which(membership == k)
-  x <- dplyr::filter(x, !!as.name(stnames[1]) %in% cluster)
+  x <- filter(x, !!as.name(stnames[1]) %in% cluster)
 
   # Spatio-temporal dimenstion in long format
   dims <- expand_dimensions(x)
---
+++ R/sfclust.R
@@ -0,0 +1,13 @@
+#' @title sfclust: Bayesian Spatial Functional Clustering
+#'
+#' @docType package
+#' @name sfclust
+#'
+#' @description
+#' The \code{sfclust} package
+#'
+#' @author Ruiman Zhong, \email{ruiman.zhong@kaust.edu.sa}
+#' @author Erick A. Chacon-Montalvan \email{erick.chaconmontalvan@kaust.edu.sa}
+#' @author Paula Moraga, \email{paula.moraga@kaust.edu.sa}
+#'
+"_PACKAGE"
