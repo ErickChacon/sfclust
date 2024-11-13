ff1c22efa8756981240386d84cfc0f2cfe18e4f6
Erick <eral.th07@gmail.com>
2024-11-13 12:41:03 +0300

    3 plots by default including prediction

--- R/otherfunctions.R
+++ R/otherfunctions.R
@@ -92,6 +92,61 @@ update.sfclust <- function(x, sample = nrow(x$samples$membership)) {
   x
 }
 
+#' Fitted Values for `sfclust` Objects
+#'
+#' This function calculates the fitted values for a specific clustering sample in an
+#' `sfclust` object, based on the estimated models for each cluster. The fitted
+#' values are computed using the membership assignments and model parameters
+#' associated with the selected clustering sample.
+#'
+#' @param x An object of class 'sfclust', containing clustering results and models.
+#' @param sample An integer specifying the clustering sample number for which
+#'        the fitted values should be computed. The default is the `id` of the
+#'        current clustering. The value must be between 1 and the total number
+#'        of clustering (membership) samples.
+#' @return A matrix of fitted values, where each row corresponds to a data point
+#'         and each column corresponds to a cluster.
+#' @details
+#' The function first checks if the provided `sample` value is valid (i.e., it is
+#' within the range of available clustering samples). If the specified `sample`
+#' does not match the current clustering `id`, the `sfclust` object is updated
+#' accordingly. It then retrieves the membership assignments and cluster models
+#' for the selected sample, calculates the linear predictions for each cluster,
+#' and combines them into a matrix of fitted values.
+#'
+#' @examples
+#' # Assuming 'sfclust_obj' is a pre-existing sfclust object
+#' fitted_values <- fitted.sfclust(sfclust_obj, sample = 3)
+#'
+#' @export
+fitted.sfclust <- function(x, sample = x$clustering$id) {
+  if (sample < 1 || sample > nrow(x$samples$membership)) {
+    stop("`sample` must be between 1 and the total number clustering (membership) samples.")
+  }
+  if (sample != x$clustering$id) x <- update(x, sample = sample)
+
+  membership <- x$samples$membership[sample,]
+  clusters <- 1:max(membership)
+  predlist <- lapply(
+    1:max(membership), linpred_each, membership, x$clustering$models,
+    attr(x, "args")$stdata, attr(x, "args")$stnames
+  )
+  do.call(rbind, predlist)
+}
+
+linpred_each <- function(k, membership, models, stdata, stnames){
+  df <- data_each(k, membership, stdata, stnames)
+  df$linpred <- linpred_each_corrected(models[[k]])
+  df$cluster <- k
+  df
+}
+
+# linear predictor only for terms that are defined at cluster level
+linpred_each_corrected <- function(x){
+  aux <- if ("id" %in% names(x$summary.random)) x$summary.random[["id"]]$mean else 0
+  x$summary.linear.predictor$mean - aux
+}
+
 #' Plot function for sfclust objects with a conditional legend and log marginal likelihood
 #'
 #' This function plots the map with estimated clusters from an sfclust object and adds a legend if the number of clusters is less than 10.
@@ -102,15 +157,17 @@ update.sfclust <- function(x, sample = nrow(x$samples$membership)) {
 #' @param sample The row of the cluster matrix to use for plotting (default is the last row).
 #' @param title A title for the plot (default is "Estimated Clusters").
 #' @export
-plot.sfclust <- function(x, sample = nrow(x$membership), which = c(1, 2), clusters = NULL, sort = TRUE, legend = FALSE, ...) {
+plot.sfclust <- function(x, sample = x$clustering$id, which = 1:3, clusters = NULL, sort = FALSE, legend = FALSE, ...) {
 
-  if (sample < 1 || sample > nrow(x$membership)) {
+  nsamples <- nrow(x$samples$membership)
+  if (sample < 1 || sample > nsamples) {
     stop("`sample` must be between 1 and the total number clustering (membership) samples.")
   }
 
   # get geometries, selected membership and clusters to plot
   geoms <- st_get_dimension_values(attr(x, "args")$stdata, attr(out, "args")$stnames[1])
-  membership <- if (sort) sort_membership(x$membership[sample,]) else x$membership[sample,]
+  membership <- x$samples$membership[sample,]
+  if (sort) membership <- sort_membership(x$samples$membership[sample,])
   if (is.null(clusters)) clusters <- 1:max(membership)
 
   # visualize
@@ -119,7 +176,7 @@ plot.sfclust <- function(x, sample = nrow(x$membership), which = c(1, 2), cluste
     membership[!(membership %in% clusters)] <- NA
     membership <- factor(membership)
     plot(geoms, col = membership,
-      main = paste("Clustering sample", sample, "out of", nrow(x$membership)),
+      main = paste("Clustering sample", sample, "out of", nsamples),
       xlab = "Longitude", ylab = "Latitude", border = "black", ...
     )
     if (legend) {
@@ -127,10 +184,17 @@ plot.sfclust <- function(x, sample = nrow(x$membership), which = c(1, 2), cluste
         border = "black", title = "Cluster", cex = 0.8, bty = "n")
     }
   }
+  if (3 %in% which) { # functional shapes
+    # if (sample != x$clustering$id) x <- update(x, sample = sample)
+    df <- fitted(out, sample = sample)
+    df <- subset(df, cluster %in% clusters)
+    plot(df$time, df$linpred, col = df$cluster, main = "Cluster mean functions",
+      xlab = "Time", ylab = "Cluster linear predictor", pch = 19)
+  }
   if (2 %in% which) { # marginal likelihood convergence
-    plot(x$log_mlike, type = "l", main = "Log marginal likelihood convergence",
+    plot(x$samples$log_mlike, type = "l", main = "Log marginal likelihood convergence",
       xlab = "Sample", ylab = "Log marginal likelihood", col = "blue", lwd = 2)
-    points(sample, x$log_mlike[sample], col = "red", pch = 19)
+    points(sample, x$samples$log_mlike[sample], col = "red", pch = 19)
   }
   if (length(which) > 1) par(opar)
 
