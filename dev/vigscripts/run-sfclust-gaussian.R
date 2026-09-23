370f5272fd49fc43a3f584936ca09113ff97a7f1
Erick <eral.th07@gmail.com>
2026-09-23 14:11:41 +0200

    update advanced features vignette

--- dev/vigscripts/run-sfclust-gaussian.R
+++ dev/vigscripts/run-sfclust-gaussian.R
@@ -2,26 +2,24 @@ library(sfclust)
 library(stars)
 
 set.seed(123)
-initial_cluster <- genclust(stgaus, nclust = 20)
+formula <- y ~ f(id_time, model = "rw1",
+  hyper = list(prec = list(prior = "normal", param = c(-2, 1))))
 
 system.time(
-  result <- sfclust(stgaus, graphdata = initial_cluster, logpen = -50,,
-    formula = y ~ f(id_time, model = "rw1",
-                    hyper = list(prec = list(prior = "normal", param = c(-2, 1)))),
+  result <- sfclust(stgaus, 20, formula = formula, logpen = -50,
     niter = 50, burnin = 10, thin = 2, nmessage = 10,
     path_save = file.path("dev", "vigdata", "full-gaussian-mcmc1.rds"))
 )
 #    user  system elapsed
-# 331.552  60.037  58.307
+#  58.105  37.554  73.656
 
 system.time(
   result2 <- update(
     result, niter = 1000, nsave = 500,
-    path_save = file.path("dev", "vigdata", "full-gaussian-mcmc2.rds")
-  )
+    path_save = file.path("dev", "vigdata", "full-gaussian-mcmc2.rds"))
 )
 #     user   system  elapsed
-# 4685.726  859.919  830.404
+# 712.609  463.280  903.903
 
 # Reduce size of objects
 result1 <- readRDS(file.path("dev", "vigdata", "full-gaussian-mcmc1.rds"))
